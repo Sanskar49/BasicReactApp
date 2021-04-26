1d865b9e267a11964436c510ba760e96a0a1c062
Sanskar B.C <sanskarbc14@gmail.com>
2021-04-26 16:16:47 +0545

    First commit, learning class Component

---
+++ src/components/ComponentOne.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './ComponentOne.css'
+
+class ComponentOne extends React.Component {
+    render() {
+        return (
+            <div className="demo">
+                <h1>One</h1>
+                <h2>Two</h2>
+            </div>
+        );  
+    }
+}
+export default ComponentOne;
--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import ComponentOne from './components/ComponentOne'
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  
+    <ComponentOne />
+    
+  ,
   document.getElementById('root')
 );
 

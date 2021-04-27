bd445cfe89fec97236371fb85feaf0ac9c19b8fe
Sanskar B.C <sanskarbc14@gmail.com>
2021-04-27 15:04:49 +0545

    Counter type made with states and props logic

--- src/components/ComponentOne.js
+++ src/components/ComponentOne.js
@@ -2,11 +2,41 @@ import React from 'react';
 import './ComponentOne.css'
 
 class ComponentOne extends React.Component {
+      constructor(props) {
+          super(props);
+          this.state = {
+              value: 0
+          }
+          this.inc = this.inc.bind(this);
+          this.dec = this.dec.bind(this);
+      }
+      
+
+      inc() {
+          this.setState({value: this.state.value+1});
+      }
+      dec() {
+          if(this.state.value<=0) {
+              return;
+          }
+          else {
+          this.setState({value: this.state.value -=1});
+          }
+          
+        }
+
     render() {
+       
+        
         return (
+            
             <div className="demo">
-                <h1>One</h1>
-                <h2>Two</h2>
+                <p className="demo">{this.state.value}</p>
+                <br />
+                <button onClick = {this.inc}>Increament</button>
+                <button onClick = {this.dec}>Decreament</button>
+                
+
             </div>
         );  
     }
